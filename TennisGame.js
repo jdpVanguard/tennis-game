b381fb38cb1e238b4120b4a4e42556fb3227fc76
jdpVanguard <61425333+jdpVanguard@users.noreply.github.com>
2020-12-04 13:30:48 -0500

    initial commit

---
+++ TennisGame.js
@@ -0,0 +1,16 @@
+var TennisGame = function(p1N, p2N) {
+
+};
+
+TennisGame.prototype.getScore = function() {
+	return "TODO.  Please implement.";
+};
+
+TennisGame.prototype.wonPoint = function(playerName) {
+    
+
+};
+
+if (typeof window === "undefined") {
+    module.exports = TennisGame;
+}
